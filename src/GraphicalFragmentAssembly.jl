548f020bb54fa57ec16c72f3c2ad2fb42aa4c93f
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2023-02-22 10:55:23 +0100

    Working prototype

---
+++ src/GraphicalFragmentAssembly.jl
@@ -0,0 +1,227 @@
+module GraphicalFragmentAssembly
+
+using StringViews: StringView
+using TranscodingStreams: TranscodingStream, NoopStream
+using Automa: Automa, RegExp as RE
+using Automa.RegExp: @re_str
+using Automa.Stream: @mark, @markpos
+using BioGenerics: BioGenerics
+
+# TODO: Make better context - goto, maybe simd maybe with unroll?
+const AUTOMA_CONTEXT = Automa.CodeGenContext(;
+    vars=Automa.Variables(:p, :p_end, :p_eof, :ts, :te, :cs, :data, :mem, :byte)
+)
+
+struct Unsafe end
+const unsafe = Unsafe()
+
+"""
+    LazyRecord
+
+A semi-parsed GFA record. Only the record type and the location of the optional data
+is parsed. Use `rectype` to get the record type, `materialize` to create a real record object,
+and `optionals` to get a lazy iterator of `LazyOptional` fields.
+"""
+mutable struct LazyRecord
+    # The optional data is copied in here raw with tabs
+    buffer::Vector{UInt8}
+    # First integer in optional is the final non-optional byte + 2
+    # E.g. in line "H\tAB:i:5", optional is 3:8.
+    # For missing optional, it's 1:0
+    optional::UnitRange{Int}
+    type::UInt8
+end
+
+# Empty lazy records are comments by default
+LazyRecord() = LazyRecord(UInt8[], 1:0, UInt8('#'))
+
+function empty!(x::LazyRecord)
+    x.optional = 1:0
+    x.type = UInt8('#')
+end
+
+Base.copy(x::LazyRecord) = LazyRecord(copy(x.buffer), x.optional, x.type)
+
+rectype(x::LazyRecord) = unsafe_char(x.type)
+
+# Unsafe in that the byte must be ASCII for this to be valid
+unsafe_char(x::UInt8) = reinterpret(Char, (x % UInt32) << 24)
+
+function append_from!(dst, dpos, src, spos, n)
+    if length(dst) < dpos + n - 1
+        resize!(dst, dpos + n - 1)
+    end
+    # TODO: Could add unsafe_copyto! here
+    copyto!(dst, dpos, src, spos, n)
+    return dst
+end
+
+function materialize(record::LazyRecord)
+    if record.type == UInt8('H')
+        return materialize_header(record)
+    else
+        error("Invalid record type")
+    end
+end
+
+include("optional.jl")
+using .Optional
+
+GFA_MACHINE = let
+    newline = let
+        lf = re"\n"
+        lf.actions[:enter] = [:count_line]
+        RE.opt(re"\r") * lf
+    end
+    comment = re"#[^\n]*"
+
+    # This byte marks the entry into optional fields, which we have a separate
+    # machine to handle to avoid single machines being insanely complex
+    optional_entry = re"\t"
+    optional_entry.actions[:enter] = [:enter_optional]
+
+    header = re"H" * RE.opt(optional_entry)
+
+    line = header
+    line.actions[:enter] = [:enter_line]
+    line.actions[:exit] = [:exit_line]
+    line_or_comment = line | comment
+
+    gfa = line_or_comment * RE.rep(newline * line_or_comment) * RE.rep(newline)
+    Automa.compile(gfa)
+end
+
+const GFA_ACTIONS = Dict(
+    :count_line => :(linenum += 1),
+    :enter_line => quote
+        @mark
+        record.type = byte
+    end,
+    # At the end of a line, we append the entire line to the lazy record,
+    # including all tabs and so on, but not including final \r?\n
+    :exit_line => quote
+        append_from!(record.buffer, 1, data, @markpos, p - @markpos)
+        # Set bufferpos back one byte so next record begins at the same byte
+        # where the machine left off
+        buffer.bufferpos -= 1
+        found = true
+        @escape
+    end,
+    :enter_optional => quote
+        # We're at the tab now, so optional data starts at p+1
+        optional_start = p + 1
+        record_optional_index = p - @markpos() + 2
+        # Read the optional data and mark its position in the record,
+        # and also update p.
+        p = index_optional!(stream, record, optional_start, linenum, record_optional_index) - 1
+        buffer.bufferpos = p
+        # It is possible that the optional machine has resized the buffer underlying
+        # data. If that's the case, then the mem variable points to now-invalid data.
+        # So redefine it
+        mem = Automa.SizedMemory(data)
+    end
+)
+
+Automa.Stream.generate_reader(
+    :read_record!,
+    GFA_MACHINE;
+    arguments = (:(record::LazyRecord), :(linenum::Int), :(state::Int)),
+    actions = GFA_ACTIONS,
+    context = AUTOMA_CONTEXT,
+    initcode = quote
+        linenum = 1
+        byte = 0x00
+        found = false
+        empty!(record)
+        cs = state
+    end,
+    loopcode = quote
+        # TODO: Better error message, potentially using Automa's fancy new errors?
+        found && @goto __return__
+    end,
+    returncode = :(return cs, linenum, found)
+) |> eval
+
+mutable struct Reader{S <: TranscodingStream} <: BioGenerics.IO.AbstractReader
+    const stream::S
+    const record::LazyRecord
+    automa_state::Int
+    linenum::Int
+    # Current record, to be copied out if necessary
+    # Whether to automatically copy out the record
+    const copy::Bool
+
+    function Reader{T}(io::T, copy::Bool) where {T <: TranscodingStream}
+        v = Vector{UInt8}(undef, 2048)
+        @inbounds v[1] = UInt8('#')
+        record = LazyRecord(v, 1:0, UInt8('#'))
+        new{T}(io, record, 1, 1, copy)
+    end
+end
+
+Reader(io::TranscodingStream; copy::Bool=true) = Reader{typeof(io)}(io, copy)
+Reader(io::IO; kwargs...) = Reader(NoopStream(io); kwargs...)
+
+function tryread!(reader::Reader, record::LazyRecord)
+    (state, linenum, found) = read_record!(reader.stream, record, reader.linenum, reader.automa_state)
+    if !found
+        iszero(state) && return nothing
+        throw(ArgumentError(lazy"Malformed GFA file on line $linenum"))
+    end
+    reader.linenum = linenum
+    reader.automa_state = state
+    return record
+end
+
+function Base.read!(reader::Reader, record::LazyRecord)
+    y = tryread!(reader, record)
+    y === nothing ? throw(EOFError()) : record
+end
+
+function Base.iterate(reader::Reader, _::Nothing=nothing)
+    y = tryread!(reader, reader.record)
+    return if y === nothing
+        nothing
+    else
+        record = reader.copy ? copy(reader.record) : reader.record
+        (record, nothing)
+    end
+end
+
+struct Header
+    version::Union{String, Nothing}
+end
+
+function materialize_header(record::LazyRecord)
+    tag = convert(Tag, "VN")
+    version = nothing
+    for (k, v) in optionals(record)
+        k == tag && materialize(v)::String
+    end
+    Header(version)
+end
+
+#=
+using GraphicalFragmentAssembly
+using TranscodingStreams
+
+data = "#abcdef\r\nH\tVS:i:-199\nH\nH\tAB:A:z"
+rec = GraphicalFragmentAssembly.LazyRecord()
+io = NoopStream(IOBuffer(data); bufsize=3)
+
+GraphicalFragmentAssembly.read_record!(io, rec, 1, 1)
+GraphicalFragmentAssembly.read_record!(io, rec, 1, 4)
+=#
+
+#=
+using GraphicalFragmentAssembly
+using TranscodingStreams
+v = UInt8[]
+data = "VN:Z:somedata\tXA:i:-1991\tKX:f:-19.33E-21\tAA:A:z\nSOMEMOREDATA"
+
+io = NoopStream(IOBuffer(data))
+GraphicalFragmentAssembly.append_optional!(io, v, 1)
+=#
+
+
+end # module
---
+++ src/optional.jl
@@ -0,0 +1,206 @@
+module Optional
+
+# TODO: Add validation of only 1 of same Optional in same line
+
+using GraphicalFragmentAssembly: Unsafe, unsafe, LazyRecord, AUTOMA_CONTEXT, append_from!, unsafe_char
+import GraphicalFragmentAssembly: materialize
+using Automa: Automa, RegExp as RE
+using Automa.RegExp: @re_str
+using Automa.Stream: @mark, @markpos
+using StringViews: StringView
+
+export Tag, OptionalFields, optionals, LazyOptional, JSONString, index_optional!
+
+const B_ARRAY_TYPES_DICT = Dict(
+    'c' => Int8,
+    'C' => UInt8,
+    's' => Int16,
+    'S' => UInt16,
+    'i' => Int32,
+    'I' => UInt32,
+    'f' => Float32
+)
+
+const BArrayTypes = Union{values(B_ARRAY_TYPES_DICT)...}
+
+const OptTypes = Union{Char, Int, Float32, String, Vector{<:BArrayTypes}}
+
+# Technically, adding more parameters to Base Julian types is not breaking, but if some genious
+# decides to do it in the future, this type turns into a UnionAll type, which will wreck
+# the performance of all containers that store views, so please don't do this.
+const ViewType = SubArray{UInt8, 1, Vector{UInt8}, Tuple{UnitRange{Int64}}, true}
+@assert typeof(view([0x61], 1:1)) == ViewType
+
+struct Tag <: AbstractString
+    x::NTuple{2, UInt8}
+
+    Tag(x::NTuple{2, UInt8}, ::Unsafe) = new(x)
+end
+
+function Tag(x::NTuple{2, UInt8})
+    a, b = x
+    if !(a in UInt8('A'):UInt8('Z') || a in UInt8('z'):UInt8('z')) ||
+       !(b in UInt8('A'):UInt8('Z') || b in UInt8('z'):UInt8('z') || b in UInt8('0'):UInt8('9'))
+       error("Tag must match regex [A-Za-z][A-Za-z0-9]")
+    end
+    Tag(x, unsafe)
+end
+
+function Tag(s::Union{String, SubString{String}})
+    ncodeunits(s) == 2 || error("String must have 2 codeunits")
+    Tag((@inbounds codeunit(s, 1), @inbounds codeunit(s, 2)))
+end
+Base.convert(::Type{Tag}, x::Union{String, SubString{String}}) = Tag(x)
+function Base.convert(::Type{Tag}, x::NTuple{2, T}) where {T <: Union{UInt8, Char}}
+    Tag((UInt8(x[1]), UInt8(x[2])))
+end
+
+Base.String(x::Tag) = string(unsafe_char(x.x[1]), unsafe_char(x.x[2]))
+Base.convert(::Type{String}, x::Tag) = String(x)
+
+Base.codeunits(x::Tag) = x.x
+Base.ncodeunits(::Tag) = 2
+Base.isvalid(::Tag, i::Integer) = i == 1 || i == 2
+Base.length(::Tag) = 2
+Base.iterate(s::Tag, i::Int=1) = i > 2 ? nothing : (unsafe_char(s.x[i]), i+1)
+
+struct LazyOptional
+    type::UInt8
+    data::ViewType
+end
+
+function Base.show(io::IO, x::LazyOptional)
+    print(io, "LazyOptional(", repr(Char(x.type)), ", ", x.data, ')')
+end
+
+LazyOptional(type::Char, data::Vector{UInt8}) = LazyOptional(UInt8(type), view(data, 1:length(data)))
+
+struct OptionalFields <: AbstractDict{Tag, LazyOptional}
+    x::LazyRecord
+end
+optionals(x::LazyRecord) = OptionalFields(x)
+
+function materialize(x::OptionalFields)
+    result = Dict{Tag, OptTypes}()
+    for (k, v) in x
+        haskey(result, k) && error(lazy"Duplicate optional tag \"$k\"")
+        result[k] = materialize(v)
+    end
+    result
+end
+
+function Base.length(x::OptionalFields)
+    if isempty(x.x.optional)
+        0
+    else
+        count(isequal(UInt8('\t')), view(x.x.buffer, x.x.optional)) + 1
+    end
+end
+
+Base.IteratorSize(::Type{OptionalFields}) = Base.SizeUnknown()
+
+function Base.iterate(x::OptionalFields, state=first(x.x.optional))
+    state > last(x.x.optional) && return nothing
+    buffer = x.x.buffer
+    start_pos = state + 5
+    final_pos = let
+        p = findnext(isequal(UInt8('\t')), buffer, state+6)
+        p === nothing ? last(x.x.optional) : (p - 1)
+    end
+    data = view(buffer, start_pos:final_pos)
+    tag = Tag((buffer[state], buffer[state+1]), unsafe)
+    lazytag = LazyOptional(buffer[state+3], data)
+    (tag => lazytag, final_pos + 2)
+end
+
+function Base.get(x::OptionalFields, k, default)
+    key = convert(Tag, k)
+    for (tag, value) in x
+        tag == key && return value
+    end
+    return default
+end
+
+function Base.getindex(x::OptionalFields, k)
+    key = convert(Tag, k)
+    y = get(x, key, nothing)
+    y === nothing && throw(KeyError(key))
+    y
+end
+
+struct JSONString
+    x::String
+end
+
+function materialize(tag::LazyOptional)::OptTypes
+    type = tag.type
+    data = tag.data
+    if type == UInt8('A')
+        unsafe_char(data[1])
+    elseif type == UInt8('i')
+        parse(Int, StringView(data); base=10)
+    elseif type == UInt8('f')
+        parse(Float32, StringView(data))
+    elseif type == UInt8('Z')
+        String(data)
+    elseif type == UInt8('J')
+        JSONString(String(data))
+    elseif type == UInt8('H')
+        hex2bytes(data)
+    elseif type == UInt8('B')
+        T = B_ARRAY_TYPES_DICT[unsafe_char(data[1])]
+        s = StringView(view(data, 2:lastindex(data)))
+        [parse(T, i) for i in eachsplit(s, ',')]::Vector{<:BArrayTypes}
+    end
+end
+
+const OPTIONAL_MACHINE = let
+    signed = re"[-+]?[0-9]+"
+    unsigned = re"\+?[0-9]+"
+    float = re"[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?"
+    string = re"[ !-~]+"
+
+    sint_B = re"[csi]" * RE.rep1(re"," * signed)
+    uint_B = re"[CSI]" * RE.rep1(re"," * unsigned)
+    float_B = re"f" * RE.rep1(re"," * float)
+    val_B = sint_B | uint_B | float_B
+
+    field = re"[A-Za-z][A-Za-z0-9]:" * RE.alt(
+        re"A:[!-~]",
+        re"i:" * signed,
+        re"f:" * float,
+        re"[ZJ]:" * string,
+        re"H:[0-9A-F]+",
+        re"B:" * val_B
+    )
+
+    line = field * RE.rep(re"\t" * field)
+    line.actions[:exit] = [:exit_machine]
+    Automa.compile(line * RE.opt(re"[\r\n]"))
+end
+
+Automa.Stream.generate_reader(
+    :index_optional!,
+    OPTIONAL_MACHINE;
+    arguments = (:(record::LazyRecord), :(from::Int), :(linenum::Int), :(record_index::Int)),
+    context = AUTOMA_CONTEXT,
+    actions = Dict(:exit_machine => quote
+        optional_length = p - from + p_decremented
+        record.optional = record_index:record_index+optional_length-1
+        return p
+    end),
+    initcode = quote
+        byte = 0x00
+        p_decremented = 0
+        buffer.bufferpos = from
+    end,
+    # The Base.skip in the generated code can decrement bufferpos, after which
+    # p is decremented by the same amount.
+    # Keep track of how much this is, so we know how many bytes the optional data is
+    loopcode = quote
+        cs < 0 && error(lazy"Error when parsing optional fields on line $linenum")
+        p_decremented += p - buffer.bufferpos
+    end
+) |> eval
+
+end
